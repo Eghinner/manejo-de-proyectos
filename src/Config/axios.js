ebe7dd9bad8b801889d17e6305093106a404c3b8
Eghinner <eghinner@gmail.com>
2021-11-18 19:58:03 -0800

    .

--- src/Config/axios.js
+++ src/Config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const ClienteAxios = axios.create({
-	baseURL: 'https://safe-citadel-23578.herokuapp.com/'
+	baseURL: process.env.REACT_APP_BACKEND_URL
 })
 
 export default ClienteAxios
